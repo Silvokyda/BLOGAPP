641bf88fdd59934f9ccb9f26318375d9ceb0b262
silvo <silvo@localhost.localdomain>
2024-08-22 16:43:56 +0300

    components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
+import Featured from "./componets/featured/featured";
+import CategoryList from "./componets/categoryList/categoryList";
+import CardList from "./componets/cardList/cardList";
+import Menu from "./componets/menu/menu";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
+      </div>
+    </div>
+  );
 }
